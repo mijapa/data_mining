4f8127c172937a8f63e062493697db0446a898f4
Michał <michaljanpatyk@gmail.com>
2020-04-27 13:55:44 +0200

    add filling missing values

--- lab3/lab.m
+++ lab3/lab.m
@@ -1,6 +1,10 @@
 T = importfile("data.csv");
 figure('Name','Data')
 plot(T.Period, T.USdollarEUR);
+dollar = T.USdollarEUR;
+nanx = isnan(dollar);
+%dane zawierają braki - zostaną zastąpione średnią kroczącą k=5
+T.USdollarEUR = fillmissing(T.USdollarEUR, 'movmean', 5);
 
 przyrosty = diff(T.USdollarEUR);
 figure('Name','Przyrosty')
@@ -10,3 +14,6 @@ k5mean = movmean(T.USdollarEUR, 5);
 figure('Name','Średnia krocząca, k=5')
 plot(k5mean);
 %widać zmiany średniej - dane nie są stacjonarne
+
+
+
