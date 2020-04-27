e5ea4ed8a46faf56e67215564c8bf22316a18e11
Michał <michaljanpatyk@gmail.com>
2020-04-27 13:33:00 +0200

    add diff and k5mean plots

--- lab3/lab.m
+++ lab3/lab.m
@@ -1,2 +1,12 @@
 T = importfile("data.csv");
+figure('Name','Data')
 plot(T.Period, T.USdollarEUR);
+
+przyrosty = diff(T.USdollarEUR);
+figure('Name','Przyrosty')
+plot(przyrosty);
+
+k5mean = movmean(T.USdollarEUR, 5);
+figure('Name','Średnia krocząca, k=5')
+plot(k5mean);
+%widać zmiany średniej - dane nie są stacjonarne
