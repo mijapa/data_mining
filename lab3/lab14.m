0e883b9b70ecf5691d92caf02cb77443f3aeba5b
Michał <michaljanpatyk@gmail.com>
2020-04-27 19:03:27 +0200

    add part 14

---
+++ lab3/lab14.m
@@ -0,0 +1,19 @@
+n = 1;
+
+x=linspace(0,numel(T.USdollarEUR)-1,numel(T.USdollarEUR));
+p = polyfit(x, T.USdollarEUR, n);
+newY = polyval(p, x);
+figure('Name','Aproksymacja wielomianem stopnia 1')
+plot(x,T.USdollarEUR,x,newY,'-r');
+grid;
+legend('Data','Fitted Data');
+
+n = 3;
+
+x=linspace(0,numel(T.USdollarEUR)-1,numel(T.USdollarEUR));
+p = polyfit(x, T.USdollarEUR, n);
+newY = polyval(p, x);
+figure('Name','Aproksymacja wielomianem stopnia 3')
+plot(x,T.USdollarEUR,x,newY,'-r');
+grid;
+legend('Data','Fitted Data');
